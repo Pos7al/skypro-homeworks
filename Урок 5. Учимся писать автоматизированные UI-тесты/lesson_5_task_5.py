3e4ede5e5aa8da2aa90ad0e77f50f9d3c6ca13c5
Pos7al <anton.emeliyanov@gmail.com>
2024-06-30 12:54:32 +0300

    PEP8

--- Урок 5. Учимся писать автоматизированные UI-тесты/lesson_5_task_5.py
+++ Урок 5. Учимся писать автоматизированные UI-тесты/lesson_5_task_5.py
@@ -14,18 +14,26 @@ count = 0
 
 # Кликнуть на синюю кнопку:
 blue_button = chrome.find_element(
-    By.XPATH, "//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]").click()
+    By.XPATH,
+    "//button[contains(concat(' ', normalize-space(@class), ' '), "
+    "' btn-primary ')]").click()
 blue_button = firefox.find_element(
-    By.XPATH, "//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]").click()
+    By.XPATH,
+    "//button[contains(concat(' ', normalize-space(@class), ' '), "
+    "' btn-primary ')]").click()
 chrome.switch_to.alert.accept()
 firefox.switch_to.alert.accept()
 
 # Кликнуть на синюю кнопку 3 раза:
 for x in range(3):
     blue_button = chrome.find_element(
-        By.XPATH, "//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]").click()
+        By.XPATH,
+        "//button[contains(concat(' ', normalize-space(@class), ' '), "
+        "' btn-primary ')]").click()
     blue_button = firefox.find_element(
-        By.XPATH, "//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]").click()
+        By.XPATH,
+        "//button[contains(concat(' ', normalize-space(@class), ' '), "
+        "' btn-primary ')]").click()
     sleep(2)
     chrome.switch_to.alert.accept()
     firefox.switch_to.alert.accept()
